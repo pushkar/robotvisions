d65e38b88a3c8d9fe47e1794792700c665c2e894
pushkar <pushkarkolhe@gmail.com>
2014-09-30 17:51:07 -0400

    changed front page

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>robotvisions</title>
+  <link href="https://s3.amazonaws.com/static-style/dist/css/bootstrap-theme.min.css" rel="stylesheet">
  </head>
  <body>
- <ul>
-	<li><a href="4641/">CS 4641 Machine Learning (Summer 2013)</a></li>
-	<li><a href="3600/">CS 3600 Introduction to Artifiial Intelligence (Summer 2014)</a></li>
- </ul> 
+     <div class="container">
+            <nav class="navbar navbar-default navbar-static-top" role="navigation">
+                <ul class="nav navbar-nav">
+                <li><a href="http://www.robotvisions.org/">Home</a></li>
+                <li><a href="http://www.pushkar.name/">Pushkar Kolhe</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="container">
+         <ul>
+        	<li><a href="4641/">CS 4641 Machine Learning (Summer 2013)</a></li>
+        	<li><a href="3600/">CS 3600 Introduction to Artifiial Intelligence (Summer 2014)</a></li>
+         </ul>
+     </div>
  </body>
-</html>	
+</html>
