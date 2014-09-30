bc2346d316df27fcf64ed0495eff4475f3cda457
pushkar <pushkarkolhe@gmail.com>
2014-09-30 02:36:42 -0400

    adds links to class website

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <ul>
+	<li><a href="4641/">CS 4641 Machine Learning (Summer 2013)</a></li>
+	<li><a href="3600/">CS 3600 Introduction to Artifiial Intelligence (Summer 2014)</a></li>
+ </ul> 
  </body>
 </html>	
